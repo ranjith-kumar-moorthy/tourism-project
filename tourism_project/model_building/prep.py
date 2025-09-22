c736a43d2aa08ef8d8ac713db428edaff8f698c3
ranjith-kumar-moorthy <ranjithmooorthy@gmail.com>
2025-09-22 14:38:42 +0000

    first commit

--- tourism_project/model_building/prep.py
+++ tourism_project/model_building/prep.py
@@ -13,7 +13,7 @@ from huggingface_hub import login, HfApi
 # Define constants for the dataset and output paths
 
 
-os.environ["HF_TOKEN"] = "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"   # please use your token
+os.environ["HF_TOKEN"] = "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"   # please use your token
 api = HfApi(token=os.getenv("HF_TOKEN"))
 
 # please create your dataset as you create your space
