ec5621a957793b03d1164cc702efbfd3da1e7c92
ranjith-kumar-moorthy <ranjithmooorthy@gmail.com>
2025-09-23 04:49:08 +0000

    first commit

---
+++ tourism_project/hosting/hosting.py
@@ -0,0 +1,13 @@
+from huggingface_hub import HfApi
+import os
+
+os.environ["HF_TOKEN"] = "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"   # please use your token
+api = HfApi(token=os.getenv("HF_TOKEN"))
+api.upload_folder(
+    folder_path="tourism_project/deployment",     # the local folder containing your files
+    # replace with your repoid
+    repo_id="ranjithkumarsundaramoorthy/tourism-project-UI",          # the target repo
+
+    repo_type="space",                      # dataset, model, or space
+    path_in_repo="",                          # optional: subfolder path inside the repo
+)
